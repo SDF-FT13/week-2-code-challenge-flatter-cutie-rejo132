0c66b6bf5346010f91ee11bb655b2baa41c01719
rejo132 <enoch.luke1@student.moringaschool.com>
2025-03-24 12:00:57 +0300

    adds DOM elements

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 // Base URL for the API
 const BASE_URL = "http://localhost:3000/characters";
 
+// DOM Elements
+const characterBar = document.getElementById("character-bar");
+const detailedInfo = document.getElementById("detailed-info");
+const votesForm = document.getElementById("votes-form");
+const votesInput = document.getElementById("votes");
+const resetButton = document.getElementById("reset-btn");
+const characterForm = document.getElementById("character-form");
